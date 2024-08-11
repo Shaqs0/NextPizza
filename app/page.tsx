71b304b4e4b0d82ee3812e5f1230f28e2d1d04ae
Shaqs0 <hhgfhhhggb@gmail.com>
2024-08-11 11:12:47 +0300

    add categories and title

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import { Container, Title } from "@/components/shared";
+import { Categories, Container, Title } from '@/components/shared';
 
 export default function Home() {
-  return ( <>
+	return ( <>
 
-   <Container className="mt-10">
-      <Title text="Все пиццы" size="lg" className="font-extrabold"/>
-   </Container>
+		<Container className="mt-10">
+			<Title text="Все пиццы" size="lg" className="font-extrabold"/>
+
+	<Categories/>
+		</Container>
   
-  </>
-  )
+	</>
+	);
 }
